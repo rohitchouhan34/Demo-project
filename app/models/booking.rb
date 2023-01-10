218a630a9ef2458d68e781dc997ecc37c76eea0d
rohitchouhan34 <rohit.chouhan34@gmail.com>
2023-01-10 11:48:53 +0530

    method commented

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,9 +3,9 @@ class Booking < ApplicationRecord
   belongs_to :cab
   before_create :add_user_driver_name
 
-  private
-  def add_user_driver_name
-    self.driver_name = self.cab.driver_name
-    self.user_name = self.user.name
-  end
+  # private
+  # def add_user_driver_name
+  #   self.driver_name = self.cab.driver_name
+  #   self.user_name = self.user.name
+  # end
 end
